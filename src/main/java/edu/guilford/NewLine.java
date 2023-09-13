496d58534a49f141e20c153f53977f7f109bc22f
Apex-Incarna <emenser05@gmail.com>
2023-09-12 20:12:00 -0400

    Started reading chapter 4 Think Java

---
+++ src/main/java/edu/guilford/NewLine.java
@@ -0,0 +1,17 @@
+package edu.guilford;
+
+public class NewLine {
+    public static void newLine() {
+        String message = "Hello there";
+        System.out.println(message);
+    }
+
+
+    public static void main(String[] args) {
+        // A place for testing Java code
+        System.out.println("First line");
+        newLine();
+        System.out.println("Final line");
+
+    }
+}
--- src/main/java/edu/guilford/Sandbox.java
+++
@@ -1,8 +0,0 @@
-package edu.guilford;
-
-public class Sandbox {
-    public static void main(String[] args) {
-        // A place for testing Java code
-        
-    }
-}
